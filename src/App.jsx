ed290cc9df09c958de13f1bad4a3d991a538eb99
Faridbhp <farid.bhp9431@gmail.com>
2024-10-11 20:18:30 +0700

    add navigate to page dashboard and portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/WebAdmin/pages/Login";
+import Register from "./pages/WebAdmin/pages/Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WelcomePage from "./pages/WelcomePage";
-import ForgetPassword from "./pages/ForgetPassword";
-import ResetPassword from "./pages/ResetPassword";
+import WelcomePage from "./pages/WebAdmin/pages/WelcomePage";
+import ForgetPassword from "./pages/WebAdmin/pages/ForgetPassword";
+import ResetPassword from "./pages/WebAdmin/pages/ResetPassword";
+import Dashboard from "./pages/WebAdmin/pages/Dashboard";
+import Portofolio from "./pages/PortoWeb/pages/Portofolio";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/forgetPassword" element={<ForgetPassword />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/portfolio" element={<Portofolio />} />
         {/* Tambahkan rute lain di sini */}
       </Routes>
     </Router>
